d9f214c4e9a9896775db501057a76b7a5c36cf5c
kelvinwambua <shwiftykelvin@gmail.com>
2025-06-20 11:20:20 +0300

    Try Five

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -4,6 +4,6 @@ def selection_sort(arr):
         for j in range(i + 1, len(arr)):
             if arr[j] < arr[min_index]:
                 min_index =j
-    if min_index != i:
-        arr[i], arr[min_index] = arr[min_index], arr[i]
+        if min_index != i:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
     return arr
