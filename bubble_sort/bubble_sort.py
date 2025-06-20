b821f7fcece9c653d604530a20075bfee2be0097
kelvinwambua <shwiftykelvin@gmail.com>
2025-06-20 11:09:04 +0300

    Try Two

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,8 +1,12 @@
 def bubble_sort(unsorted_list):
-    for i  in range(len(unsorted_list) - 1):
+    for i in range(len(unsorted_list) - 1):
+        switched  =  False
         for j in range(len(unsorted_list) - 1 - i):
-            if unsorted_list[j] > unsorted_list[i]:
-                unsorted_list[j], unsorted_list[i]= unsorted_list[i], unsorted_list[j]
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
+                switched = True
+        if not switched:
+            break
     return unsorted_list
-
     
+
