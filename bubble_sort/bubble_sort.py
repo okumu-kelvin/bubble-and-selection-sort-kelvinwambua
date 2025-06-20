0f3464057952a805a7e37b75c005f8e0a5a9f32e
kelvinwambua <shwiftykelvin@gmail.com>
2025-06-20 11:03:51 +0300

    Try One

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,8 @@
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+    for i  in range(len(unsorted_list) - 1):
+        for j in range(len(unsorted_list) - 1 - i):
+            if unsorted_list[j] > unsorted_list[i]:
+                unsorted_list[j], unsorted_list[i]= unsorted_list[i], unsorted_list[j]
+    return unsorted_list
+
+    
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,9 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+    for i in range(len(arr) -1):
+        min_index = i
+        for j in range(i + 1, len(arr)-1):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        if min_index != i:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
